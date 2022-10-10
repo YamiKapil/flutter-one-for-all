9be3e4028f681df67c39690c4eae9c57722399fe
YamiKapil <yamikapil@gmail.com>
2022-10-10 17:06:28 +0545

    animation added in navigation button

--- lib/common_components/navigation_button.dart
+++ lib/common_components/navigation_button.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class NavigationButton extends StatelessWidget {
+class NavigationButton extends StatefulWidget {
   final String screenName;
   final Widget navigationScreen;
   const NavigationButton({
@@ -10,17 +12,49 @@ class NavigationButton extends StatelessWidget {
   });
 
   @override
+  State<NavigationButton> createState() => _NavigationButtonState();
+}
+
+class _NavigationButtonState extends State<NavigationButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => navigationScreen,
-          ),
-        );
-      },
-      child: Text(screenName),
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) => Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateY(360 * _animationController.value * (pi / 180.0)),
+        child: child,
+      ),
+      child: ElevatedButton(
+        onPressed: () async {
+          await _animationController.forward(from: 0.0);
+          if (!mounted) return;
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => widget.navigationScreen,
+            ),
+          );
+        },
+        child: Text(widget.screenName),
+      ),
     );
   }
 }
