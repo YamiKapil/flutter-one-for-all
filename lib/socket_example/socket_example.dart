446a783372a3bca5fc391d64ae83e38279f60fd8
YamiKapil <yamikapil@gmail.com>
2022-12-24 11:22:35 +0545

    socket example added

---
+++ lib/socket_example/socket_example.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SocketExample extends StatelessWidget {
+  const SocketExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('Hello this is this'),
+        ),
+      ),
+    );
+  }
+}
